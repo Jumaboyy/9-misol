26d17fb54f627059f1bc2e9045a3dbc6fabfdc58
Jumaboy <jumaboynematov280@gmail.com>
2024-03-01 00:00:16 +0500

    9misol

---
+++ vazifa3.py
@@ -0,0 +1,148 @@
+# masala1
+# Butin son benlgan Agar benlgan son musbat bolsa.
+# 1 ga oshinlsin aks holda ozgartmlmasm.
+# Hosil bolgan sonm ekrangachiqaruvch programma tuzilsin
+# son=int(input('Sonni kiriting: '))
+# if son>0:
+#     natija=son+1
+#     print(f'Natija: {natija}')
+# else:
+#     print(f'Natija: {son}')
+
+# masala2
+# Butun son benlgan Agar benigan son musbat bolsa.
+# 1 ga oshnng aks holda 2 ga kamaytinng
+# Hosilbolgansonni ekranga chqaruvch programma tuzilsm
+# son=int(input('Sonni kiriting: '))
+# if son>0:
+#     natija=son+1
+# else:
+#     natija=son-2
+# print(f'Natija: {natija}')
+
+# masala3
+# Butun son benlgan Agar benlgan son musbat bolsa.
+# 1 ga oshmng agar manfiy bo lsa 2 ga kamaytmng Agar 0 ga teng bo lsa.
+# # 10 ni o zlashtirsin Hosil bo lgan sonni ekranga chqaruvch programma tuzilsm
+# son=int(input('Sonni kiriting: '))
+# if son>0:
+#     natija=son+1
+# elif son<0:
+#     natija=son-2
+# else:
+#     natija=10
+# print(f'Natija: {natija}')
+
+# masala4
+# Uchta butun son benlgan Shu sortar orasidan nechta musbat son bortgim amqlovch programma tuzjlsin
+# son1=int(input('1 Sonni kiriting: '))
+# son2=int(input('2 Sonni kiriting: '))
+# son3=int(input('3 Sonni kiriting: '))
+# if son1>0:
+#     son=1
+# if son2>0:
+#     son=son+1
+# if son3>0:
+#     son=son+1
+#
+# else:
+#     print('MUsbat son kiritng')
+# print(f'Musbat son; {son}')
+
+# masala5
+# Uchta butun son benlgan Shu sortar orasidan nechta musbat va manfiy son boriigim amqlovch programma tuzilsm
+# son1=int(input('1 Sonni kiriting: '))
+# son2=int(input('2 Sonni kiriting: '))
+# son3=int(input('3 Sonni kiriting: '))
+# manfiy=3
+# if son1>0:
+#     son=1
+#     manfiy=manfiy-1
+#
+# if son2>0:
+#     son=son+1
+#     manfiy = manfiy - 1
+# if son3>0:
+#     son=son+1
+#     manfiy = manfiy - 1
+# print(f'Musbat son; {son}')
+# print(f'Manfiy son: {manfiy}')
+
+# masala6
+# ikkita butun son benlgan Shu sonlamng kattasim amqlovchi programma tuzjlsm
+# son1=int(input('1 Sonni kiriting: '))
+# son2=int(input('2 Sonni kiriting: '))
+# if son1>son2:
+#     print('son2dan, son1 katta')
+# elif son1==son2:
+#     print('Sonlar teng')
+# else:
+#     print('son1dan, son2 katta')
+
+# masala7
+# ikkita butunsonbenlgan Shusortamng kichigim tartib raqammi amqlovch programma tuzilsin
+# son1=int(input('1 Sonni kiriting: '))
+# son2=int(input('2 Sonni kiriting: '))
+# if son1>son2:
+#     print('son2dan, son1 katta kichigi son2')
+# elif son1==son2:
+#     print('Sonlar teng')
+# else:
+#     print('son1dan, son2 katta kichigi son1')
+
+# masala8
+# ikkita butun son benlgan Shu sortamng awal kattasm keyin kjchgim ekranga chqarurch programma tuzilsin
+# son1=int(input('1 Sonni kiriting: '))
+# son2=int(input('2 Sonni kiriting: '))
+# if son1>son2:
+#     print(f'kattasi: {son1}, kichigi: {son2}')
+# elif son1==son2:
+#     print('Kiritilgan sonlar teng')
+# else:
+#     print(f'kattasi: {son2}, kichigi: {son1}')
+
+# masala9
+# A va B haqiqiy sonlan benngan Shu sortami shunday o zgartmsh kerakki A son kichk B son katta bolsin A va B mng qiymati ekranga chqanlsm
+# a=int(input('A Sonni kiriting: '))
+# b=int(input('B Sonni kiriting: '))
+# print(f'A son: {b}, B son: {a}')
+
+# masala10
+# A va B butun sortan benlgan Agar ozgaruvchilar ozaro teng bolsa
+# A va B ozgaruvchilan ulammg yigindismi ozJashtrsin Agar teng bolsa.
+# 0 ni oztashtirsm A va B mng qiymati ekranga chqanlsin
+
+# a=int(input('A Sonni kiriting: '))
+# b=int(input('B Sonni kiriting: '))
+# if a==b:
+#     print('0')
+# else:
+#     print(a+b)
+
+# masala11
+# A va B butun sonlan benlgan Agar ozgaruvchlar ozaro teng bolmasa.
+# A va B bu sonlamng kattasini ozlashtirsin Agarteng bolsa.
+# 0 m ozJashtorsin A va B mngqiymati ekranga chqanlsin]
+# a=int(input('A Sonni kiriting: '))
+# b=int(input('B Sonni kiriting: '))
+# if a==b:
+#     print('0')
+# elif a<b:
+#     print('A sondan B son katta')
+# else:
+#     print('B sondan A son katta')
+
+# masala12
+# Uchta son benlgan Shusortami kichgmi amqlo\ch programma tuzilsin
+# son1=int(input('1 Sonni kiriting: '))
+# son2=int(input('2 Sonni kiriting: '))
+# son3=int(input('3 Sonni kiriting: '))
+# if son1>son2:
+#     if son2>son3:
+#         print('Kichigi 3 son')
+# elif son1>son3:
+#     if son3>son2:
+#         print('kIchigi 2 son')
+# else:
+#     print('Kichigi 1 son')
+
